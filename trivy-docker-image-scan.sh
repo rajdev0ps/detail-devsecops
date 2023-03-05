4a95a8f0a81d43f7924af3e8e096b25bcc999db9
rajdev0ps <raj.deolekar.ed@outlook.com>
2023-03-05 14:14:56 +0530

    ful-final

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-sudo cd /home/ubuntu/detail-devsecops
+
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
 sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.38.1 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $HOME/Library/Caches:/root/.cache/ aquasec/trivy:0.38.1 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
-
 
 
     # Trivy scan result processing
